9aa7ff7b04b7e9324c9dcb90dec8e80426aac94c
Mbackediop <49408486+Mbackediop@users.noreply.github.com>
2019-04-10 13:03:59 +0200

    exercice sur les nombres pair,impair

---
+++ exo2.py
@@ -0,0 +1,8 @@
+Ecrire un programme qui permet de savoir si le nombre saisi par lutilisateur est pair ou impair.
+
+a = int(input("enter un nombre:"))
+si a%2 == 0
+
+print("pair")
+sinon:
+print("impair")
