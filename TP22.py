47edcdc6da2e390204fe2e08bd9ce1eaeb9b1a70
Mbackediop <49408486+Mbackediop@users.noreply.github.com>
2019-04-10 18:23:10 +0200

    exo 3 du jour

---
+++ TP22.py
@@ -0,0 +1,12 @@
+import random
+y = random.randint(0,9)
+t = int(input("donner une valeur a t")
+#si la valeur donner est inferieur a y
+while t!=y :
+if t < y :
+   print("la valeur de t est inferieur a y" , t)
+#le cas contraire
+else :  
+   print("la valeur de t est superieur a y" , t)
+if t == y :
+   print("bravo felicitation")
