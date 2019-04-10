eec550ab223b48204d2eeca9120c9c909d8e9e0b
Mbackediop <49408486+Mbackediop@users.noreply.github.com>
2019-04-10 14:55:26 +0200

    Update exo2.py

--- exo2.py
+++ exo2.py
@@ -1,8 +1,5 @@
-Ecrire un programme qui permet de savoir si le nombre saisi par lutilisateur est pair ou impair.
-
-a = int(input("enter un nombre:"))
-si a%2 == 0
-
-print("pair")
-sinon:
-print("impair")
+a = int(input("enter un nombre"))
+if a%2 == 0:
+    print("pair")
+else :
+    print("impair")
