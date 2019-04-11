f883d793849fdeed8b604b412f7dc8bd7428422b
Mbackediop <49408486+Mbackediop@users.noreply.github.com>
2019-04-11 14:47:37 +0200

    nombre aleatoire

---
+++ TP4.py
@@ -0,0 +1,15 @@
+import random
+val_secret = random.randint(0,9)
+print(val_secret)
+answer = None
+
+for i in range(3):
+    
+    if answer != val_secret:
+        answer = int(input("entrer une valeur comprise entre 0 et 9 \n"))
+    if answer > val_secret:
+        print(" val-secret est inferier a la valeur donner \n")
+    elif answer > val_secret:
+        print("val-secret est superieur a la valeur donner \n")
+    else:
+        print("bravo, vous avez gagner")
